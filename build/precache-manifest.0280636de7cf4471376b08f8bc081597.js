59097367a8c6da33981695b49566de414df6256a
unknown <mogilipuram.karthik@gmail.com>
2020-06-19 14:08:16 +0530

    few changes in package.json

---
+++ build/precache-manifest.0280636de7cf4471376b08f8bc081597.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2e5f1617adc3713323d6615ddb316c8",
+    "url": "/Friends/index.html"
+  },
+  {
+    "revision": "a100be31e3a0675486cf",
+    "url": "/Friends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "b6d61c03acf7339286a5",
+    "url": "/Friends/static/css/main.e1cff913.chunk.css"
+  },
+  {
+    "revision": "a100be31e3a0675486cf",
+    "url": "/Friends/static/js/2.ca4a7502.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Friends/static/js/2.ca4a7502.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6d61c03acf7339286a5",
+    "url": "/Friends/static/js/main.843c86d8.chunk.js"
+  },
+  {
+    "revision": "d80e12f5267b152d68e0",
+    "url": "/Friends/static/js/runtime-main.e28d9ccc.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
